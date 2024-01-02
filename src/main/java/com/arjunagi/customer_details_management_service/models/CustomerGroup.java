7ae55f594601ee3f2a54da90bafc6f1fec23a137
Arjunagi A Rehman <abdul123arj@gmail.com>
2024-01-02 19:16:09 +0530

    made create customer record method in default customerService class to create and save the record in database

--- src/main/java/com/arjunagi/customer_details_management_service/models/CustomerGroup.java
+++ src/main/java/com/arjunagi/customer_details_management_service/models/CustomerGroup.java
@@ -1,5 +1,5 @@
 package com.arjunagi.customer_details_management_service.models;
 
 public enum CustomerGroup {
-    Hikeon, Chef, Developer
+    Hikeon, Chef, Developer, NA
 }
--- src/main/java/com/arjunagi/customer_details_management_service/services/imp/DefaultCustomerDetailsService.java
+++ src/main/java/com/arjunagi/customer_details_management_service/services/imp/DefaultCustomerDetailsService.java
@@ -2,10 +2,19 @@ package com.arjunagi.customer_details_management_service.services.imp;
 
 import com.arjunagi.customer_details_management_service.dtos.CustomerRequestDto;
 import com.arjunagi.customer_details_management_service.dtos.CustomerResponseDto;
+import com.arjunagi.customer_details_management_service.exception.CustomerAgeException;
+import com.arjunagi.customer_details_management_service.exception.CustomerDetailsAlreadyExistsException;
+import com.arjunagi.customer_details_management_service.mappers.CustomerDetailsMapper;
+import com.arjunagi.customer_details_management_service.models.CustomerDetails;
+import com.arjunagi.customer_details_management_service.models.CustomerGroup;
+import com.arjunagi.customer_details_management_service.models.Occupation;
 import com.arjunagi.customer_details_management_service.repository.ICustomerDetailsRepo;
 import com.arjunagi.customer_details_management_service.services.ICustomerDetailService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 @Service
 @AllArgsConstructor
@@ -17,7 +26,34 @@ public class DefaultCustomerDetailsService implements ICustomerDetailService {
      */
     @Override
     public void createCustomerDetailsRecord(CustomerRequestDto customerRequestDto) {
+        
+        if(customerDetailsRepo.findByEmail(customerRequestDto.getEmail()).isPresent())
+            throw new CustomerDetailsAlreadyExistsException("The customer with email "+customerRequestDto.getEmail()+" already exist");
 
+        if (ChronoUnit.YEARS.between(customerRequestDto.getDob(), LocalDate.now()) < 18) {
+            throw new CustomerAgeException("The customer must be older than 18");
+        }
+
+        CustomerGroup customerGroup=getCustomerGroup(customerRequestDto);
+
+        if(customerDetailsRepo.findByOccupationAndDobAndCustomerGroup(customerRequestDto.getOccupation(),customerRequestDto.getDob(),customerGroup).isPresent())
+            throw new CustomerDetailsAlreadyExistsException("The customer with occupation "+customerRequestDto.getOccupation()+" ,date of birth "+customerRequestDto.getDob()+" and Customer group "+customerGroup+" already exist");
+
+        CustomerDetails customerDetails= CustomerDetailsMapper.CustomerRequestDtoToCustomerDetails(customerRequestDto,new CustomerDetails());
+        customerDetails.setCustomerGroup(customerGroup);
+
+        customerDetailsRepo.save(customerDetails);
+
+    }
+
+    private CustomerGroup getCustomerGroup(CustomerRequestDto customerRequestDto){
+        String email=customerRequestDto.getEmail();
+        Occupation occupation=customerRequestDto.getOccupation();
+
+        if(email.substring(email.indexOf("@")).equals("@hikeon.tech"))return CustomerGroup.Hikeon;
+        if(occupation.equals(Occupation.Developer))return CustomerGroup.Developer;
+        if(occupation.equals(Occupation.Chef))return CustomerGroup.Chef;
+        return CustomerGroup.NA;
     }
 
     /**
