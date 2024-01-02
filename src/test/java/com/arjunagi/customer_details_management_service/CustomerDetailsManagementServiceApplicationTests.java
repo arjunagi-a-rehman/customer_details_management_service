ded50556670a7769c1ec2258aaf3128977a0bb5f
Arjunagi A Rehman <abdul123arj@gmail.com>
2024-01-02 17:27:16 +0530

    made the springboot project with dependency 1.webstarter,2.jpa,3.sql driver,4.lombok,5.springboot actuator,6.validation'

---
+++ src/test/java/com/arjunagi/customer_details_management_service/CustomerDetailsManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arjunagi.customer_details_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerDetailsManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
